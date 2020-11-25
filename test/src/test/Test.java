31bb9638794c14a5b6ba536b45f0b26294a46e0c
Giuseppe Gerardo Bifulco <74367506+ggbif@users.noreply.github.com>
2020-11-25 11:29:25 +0100

    Update Test.java

--- test/src/test/Test.java
+++ test/src/test/Test.java
@@ -17,6 +17,7 @@ public class Test {
     public static void main(String[] args) {
         // TODO code application logic here
         int a = 22;
+        int b = 18;
         int c = 77;
     }
     
